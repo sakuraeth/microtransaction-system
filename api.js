1833fd3fc3fc8aeef4434941b04133d4281694a3
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-11-14 01:57:30 +0200

    add optimization

--- api.js
+++ api.js
@@ -1,42 +1,50 @@
 import axios from 'axios';
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
-const CONTRACT_ABI = JSON.parse(process.env.CONTRACT_ABI);
-const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
-const WALLET_ADDRESS = process.env.WALLET_ADDRESS;
+
+const {
+  CONTRACT_ADDRESS,
+  INFURA_API_KEY,
+  CONTRACT_ABI,
+  WALLET_PRIVATE_KEY,
+  WALLET_ADDRESS,
+} = process.env;
+
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`));
-const contract = new web3.eth.Contract(CONTRACT_ABI as AbiItem[], CONTRACT_ADDRESS);
+const contract = new web3.eth.Contract(JSON.parse(CONTRACT_ABI) as AbiItem[], CONTRACT_ADDRESS);
+
+const getTransactionOptions = async (transaction) => {
+    return {
+        to: transaction._parent._address,
+        data: transaction.encodeABI(),
+        gas: await transaction.estimateGas({from: WALLET_ADDRESS}),
+        gasPrice: await web3.eth.getGasPrice()
+    };
+};
+
+const signAndSendTransaction = async (options) => {
+    const signed = await web3.eth.accounts.signTransaction(options, WALLET_PRIVATE_KEY);
+    return web3.eth.sendSignedTransaction(signed.rawTransaction);
+};
+
 const contractAPI = {
     processPayment: async (amount, toAddress) => {
         const transaction = contract.methods.transfer(toAddress, amount);
-        const options = {
-            to: transaction._parent._address,
-            data: transaction.encodeABI(),
-            gas: await transaction.estimateGas({from: WALLET_ADDRESS}),
-            gasPrice: await web3.eth.getGasPrice()
-        };
-        const signed = await web3.eth.accounts.signTransaction(options, WALLET_PRIVATE_KEY);
-        return web3.eth.sendSignedTransaction(signed.rawTransaction);
+        const options = await getTransactionOptions(transaction);
+        return signAndSendTransaction(options);
     },
     createUser: async (userName) => {
         const transaction = contract.methods.createUser(userName);
-        const options = {
-            to: transaction._parent._address,
-            data: transaction.encodeABI(),
-            gas: await transaction.estimateGas({from: WALLET_ADDRESS}),
-            gasPrice: await web3.eth.getGasPrice()
-        };
-        const signed = await web3.eth.accounts.signTransaction(options, WALLET_PRIVATE_KEY);
-        return web3.eth.sendSignedTransaction(signed.rawTransaction).then(receipt => {
-            return receipt;
-        }).catch(error => {
+        const options = await getTransactionOptions(transaction);
+        try {
+            return await signAndSendTransaction(options);
+        } catch (error) {
             throw error;
-        });
+        }
     },
     getUserData: async (userId) => {
         return contract.methods.getUserData(userId).call();
     }
 };
+
 export default contractAPI;
