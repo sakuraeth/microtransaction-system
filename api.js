7b979382ef45037c186191f4a7d81693e6fe456f
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-11-17 17:32:57 +0200

    optimize

--- api.js
+++ api.js
@@ -14,11 +14,31 @@ const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io
 const contract = new web3.eth.Contract(JSON.parse(CONTRACT_ABI) as AbiItem[], CONTRACT_ADDRESS);
 
 const getTransactionOptions = async (transaction) => {
+    const batch = new web3.BatchRequest();
+    
+    const gasEstimatePromise = new Promise((resolve, reject) => {
+        batch.add(web3.eth.estimateGas.request(
+            {from: WALLET_ADDRESS, to: transaction._parent._address, data: transaction.encodeABI()},
+            (error, gas) => error ? reject(error) : resolve(gas)
+        ));
+    });
+
+    const gasPricePromise = new Promise((resolve, reject) => {
+        batch.add(web3.eth.getGasPrice.request(
+            {},
+            (error, gasPrice) => error ? reject(error) : resolve(gasPrice)
+        ));
+    });
+
+    batch.execute();
+
+    const [gas, gasPrice] = await Promise.all([gasEstimatePromise, gasPricePromise]);
+
     return {
         to: transaction._parent._address,
         data: transaction.encodeABI(),
-        gas: await transaction.estimateGas({from: WALLET_ADDRESS}),
-        gasPrice: await web3.eth.getGasPrice()
+        gas,
+        gasPrice
     };
 };
 
@@ -43,6 +63,7 @@ const contractAPI = {
         }
     },
     getUserData: async (userId) => {
+        // Read operation can potentially be optimized with batching if there are multiple reads to be performed together
         return contract.methods.getUserData(userId).call();
     }
 };
